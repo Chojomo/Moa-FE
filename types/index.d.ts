5ad9ab748b54f956447d43a871d61f19f012c3c3
YuAhyeon <dkgus7123@naver.com>
2024-07-15 16:53:44 +0900

    feat: 공용 타입 #10

---
+++ types/index.d.ts
@@ -0,0 +1,17 @@
+export type ClassName = {
+  className?: string
+}
+
+export type Child = {
+  children?: React.ReactNode
+} & ClassName
+
+export type Size = {
+  width: number
+  height: number
+}
+
+export type IconProps = {
+  fill?: string
+} & ClassName &
+  Size
