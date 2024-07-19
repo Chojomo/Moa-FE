e229ed0b0f5e309e46c74a51780c674aa43e6a94
YuAhyeon <dkgus7123@naver.com>
2024-07-19 17:15:17 +0900

    move: marquee 컴포넌트화 #12

---
+++ components/Page/Home/Intro/Marquee.tsx
@@ -0,0 +1,12 @@
+export default function Marquee() {
+  return (
+    <div className="absolute flex whitespace-nowrap animate-marquee">
+      <p className="text-heading-bg text-[200px] font-bold">
+        JOIN US MOA! JOIN US MOA! JOIN US MOA! JOIN US MOA! JOIN US MOA!&nbsp;
+      </p>
+      <p className="text-heading-bg text-[200px] font-bold">
+        JOIN US MOA! JOIN US MOA! JOIN US MOA! JOIN US MOA! JOIN US MOA!&nbsp;
+      </p>
+    </div>
+  )
+}
