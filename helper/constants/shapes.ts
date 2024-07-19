8761720b9598f685663323ee5e192b18cb999064
YuAhyeon <dkgus7123@naver.com>
2024-07-19 17:16:15 +0900

    design: 메인 인트로 #12

--- helper/constants/shapes.ts
+++ helper/constants/shapes.ts
@@ -1,12 +1,12 @@
 export const shapes = {
   left: [
-    { src: '/images/mint-shapes.png', alt: 'mint-shapes' },
-    { src: '/images/green-shapes.png', alt: 'green-shapes' },
-    { src: '/images/blue-shapes.png', alt: 'blue-shapes' },
+    { color: 'mint', src: '/images/mint-shapes.png', alt: 'mint-shapes' },
+    { color: 'green', src: '/images/green-shapes.png', alt: 'green-shapes' },
+    { color: 'blue', src: '/images/blue-shapes.png', alt: 'blue-shapes' },
   ],
   right: [
-    { src: '/images/yellow-shapes.png', alt: 'yellow-shapes' },
-    { src: '/images/red-shapes.png', alt: 'red-shapes' },
-    { src: '/images/purple-shapes.png', alt: 'purple-shapes' },
+    { color: 'yellow', src: '/images/yellow-shapes.png', alt: 'yellow-shapes' },
+    { color: 'red', src: '/images/red-shapes.png', alt: 'red-shapes' },
+    { color: 'purple', src: '/images/purple-shapes.png', alt: 'purple-shapes' },
   ],
 }
