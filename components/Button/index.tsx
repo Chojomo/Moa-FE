73af124f1a5a122c16f07efd5ec899a760be7255
YuAhyeon <dkgus7123@naver.com>
2024-07-19 17:14:32 +0900

    update: 버튼 라벨 props 추가 #12

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,11 +1,18 @@
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  type: 'button' | 'submit' | 'reset'
+  ariaLabel?: string
   children?: React.ReactNode
-  type?: 'button' | 'submit' | 'reset'
 }
 
-export default function Button({ type = 'button', className, children, ...rest }: ButtonProps) {
+export default function Button({
+  type = 'button',
+  ariaLabel,
+  className,
+  children,
+  ...rest
+}: ButtonProps) {
   return (
-    <button type={type} className={className} {...rest}>
+    <button type={type} aria-label={ariaLabel} className={className} {...rest}>
       {children}
     </button>
   )
