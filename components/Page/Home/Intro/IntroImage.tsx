c0ac275ba5f7cd57da5d8274888568a95b983ae4
YuAhyeon <dkgus7123@naver.com>
2024-07-21 23:28:46 +0900

    design: 홈 이미지 추가 #12

---
+++ components/Page/Home/Intro/IntroImage.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { useTheme } from 'next-themes'
+import Image from 'next/image'
+
+export default function IntroImage() {
+  const { resolvedTheme } = useTheme()
+  const light = '/images/intro-light.png'
+  const dark = '/images/intro-dark.png'
+
+  return (
+    <Image
+      className="w-[260px] h-[263px] z-20 mb-[60px]"
+      width={0}
+      height={0}
+      src={resolvedTheme === 'light' ? light : dark}
+      alt="소개 이미지"
+      sizes="100vw"
+    />
+  )
+}
