a0f7fe1a3b981dfce6f020a7d1284def8d5b63b5
YuAhyeon <dkgus7123@naver.com>
2024-07-22 18:35:59 +0900

    update: 페이지 루트 컨테이너 위치 변경 #12

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,10 @@ import Next from '@/components/Arrow/Next'
 
 export default function Home() {
   return (
-    <div className="relative bg-background w-[100vw] h-[100vh] overflow-x-hidden px-[30px]">
+    <div className="relative bg-background w-[100vw] h-[100vh] overflow-x-hidden">
       <div className="h-[200vh]">
         {Pages.map(({ name, Component }) => (
-          <div
-            key={name}
-            className="w-[100%] h-[100vh] lg:flex-center lg:flex-row flex flex-col justify-center items-center lg:gap-[50px]"
-          >
-            <Component />
-          </div>
+          <Component key={name} />
         ))}
       </div>
       <Next />
