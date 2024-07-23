fd219d24751be0c197ff14833a1a140cd967c5b2
YuAhyeon <dkgus7123@naver.com>
2024-07-23 11:21:52 +0900

    design: 툴팁 추가 #12

--- components/Tooltip/ArrowTip.tsx
+++ components/Tooltip/ArrowTip.tsx
@@ -1,9 +1,53 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
 type ArrowTipProps = {
   children?: React.ReactNode
 }
 
 export default function ArrowTip({ children }: ArrowTipProps) {
-  const baseStyle = `absolute top-[56px] right-[7px] rounded-[4px] text-[12px] text-[#fff] bg-[#333]`
+  const [isFading, setIsFading] = useState(false)
+  const [isVisible, setIsVisible] = useState(true)
 
-  return <span className={`${baseStyle} tooltip tooltip-arrow`}>{children}</span>
+  useEffect(() => {
+    const fadeTimer = setTimeout(() => {
+      setIsFading(true)
+    }, 3000)
+
+    const removeTimer = setTimeout(() => {
+      setIsVisible(false)
+    }, 4000)
+
+    return () => {
+      clearTimeout(fadeTimer)
+      clearTimeout(removeTimer)
+    }
+  }, [])
+
+  const handleClick = () => {
+    setIsFading(true)
+    setTimeout(() => {
+      setIsVisible(false)
+    }, 1000)
+  }
+
+  if (!isVisible) return null
+
+  const baseStyle = `absolute bottom-[70px] left-[7px] rounded-[4px] text-[12px] text-[#fff] bg-[#333333f1] animate-float3 px-[12px] py-[10px]`
+
+  const fadeOutStyle = {
+    transition: 'opacity 1s ease-in-out',
+    opacity: isFading ? 0 : 1,
+  }
+
+  return (
+    <button
+      className={`${baseStyle} tooltip tooltip-arrow`}
+      style={fadeOutStyle}
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  )
 }
