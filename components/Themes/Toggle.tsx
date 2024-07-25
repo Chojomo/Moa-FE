1e8691200165c7d89918cd7c35f6641b82b7694a
YuAhyeon <dkgus7123@naver.com>
2024-07-25 22:03:11 +0900

    design: 토글 스타일 변경 #12

--- components/Themes/Toggle.tsx
+++ components/Themes/Toggle.tsx
@@ -5,14 +5,12 @@ import { useTheme } from 'next-themes'
 import { ThemeButton } from '../Button/ThemeButton'
 import { Icon } from '../Icon'
 
-export default function ThemeToggle() {
+export default function ThemeToggle({ size }: { size: string }) {
   const { resolvedTheme, setTheme } = useTheme()
-
-  console.log(`resolvedTheme : ${resolvedTheme}`)
 
   return (
     <ThemeButton
-      className="min-w-[55px] nav-item border-nav-border text-icon-normal"
+      className={`${size === 'min' ? 'min-w-[55px]' : 'min-w-[188px]'} nav-item border-nav-border text-icon-normal`}
       onClick={() => (resolvedTheme === 'dark' ? setTheme('light') : setTheme('dark'))}
     >
       <Icon name="Themes" width={20} height={20} fill="rgb(166,166,166)" />
