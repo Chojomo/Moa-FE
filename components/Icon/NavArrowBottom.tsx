2cad1503d72cd24cbf1f3d52b9c40dc1f09b7bc6
YuAhyeon <dkgus7123@naver.com>
2024-07-25 22:02:32 +0900

    design: nav arrow 아이콘 추가 #12

---
+++ components/Icon/NavArrowBottom.tsx
@@ -0,0 +1,22 @@
+import { IconProps } from '@/types'
+import { Svg } from './index'
+
+export default function NavArrowBottom({ className, width, height, fill = '#A6A6A6' }: IconProps) {
+  return (
+    <Svg
+      width={width || 20}
+      height={height || 13}
+      viewBox={[0, 0, 20, 13]}
+      className={className || ''}
+    >
+      <path
+        d="M1 1L8.5 8.5L16 1"
+        stroke="white"
+        strokeOpacity="0.85"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  )
+}
