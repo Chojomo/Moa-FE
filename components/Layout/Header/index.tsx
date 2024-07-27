d108e79c2192310a951dd504ad0b5dbbc31454a1
YuAhyeon <dkgus7123@naver.com>
2024-07-28 03:00:22 +0900

    design: 헤더 반응형 추가 #12

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -1,85 +1,172 @@
 'use client'
 
-import { useState, useCallback, useMemo } from 'react'
 import Link from 'next/link'
+import { useState } from 'react'
+import { usePathname } from 'next/navigation'
+import { useAuthStore } from '@/store/useAuth'
 import { Icon } from '@/components/Icon'
 import Button from '@/components/Button'
 import ThemeToggle from '@/components/Themes/Toggle'
-import { tincolorIcon } from '@/helper/constants/tintcolor'
-import { isLoginNav, notLoginNav } from '@/helper/constants/nav'
-import { useAuthStore } from '@/store/useAuth'
-
-type IconType = {
-  name: string
-  width: number
-  height: number
-}
-
-type NavItem = {
-  index: number
-  type: string
-  href?: string
-  minW?: string
-  styles?: string
-  script?: JSX.Element
-  icon?: IconType
-}
 
 export default function Header() {
-  const [activeIndex, setActiveIndex] = useState<number>(0)
+  const [isActive, setIsActive] = useState<boolean>(false)
   const { isLogin } = useAuthStore()
+  const pathname = usePathname()
 
-  const handleActive = (index: number) => {
-    if (activeIndex === index) {
-      return 'border-white-active text-white-active'
+  const handleActive = (href: string, type: 'border' | 'icon') => {
+    const classActive =
+      type === 'border' ? 'border-white-active text-white-active' : 'rgba(255, 255, 255, 0.85)'
+
+    const classNormal =
+      type === 'border' ? 'border-nav-border text-icon-normal' : 'rgb(166,166,166)'
+
+    if (pathname.includes(href) && href !== '/') {
+      return classActive
     }
-    return 'border-nav-border text-icon-normal'
+
+    if (pathname === href) {
+      return classActive
+    }
+
+    return classNormal
   }
 
-  const handleClick = useCallback((index: number) => {
-    setActiveIndex(index)
-  }, [])
+  type NavItem = {
+    href: string
+    label: string
+    icon: JSX.Element | null
+    additionalIcon: JSX.Element | null
+  }
 
-  const navList = useMemo(
-    () => (isLogin ? isLoginNav(activeIndex) : notLoginNav(activeIndex)),
-    [isLogin, activeIndex]
-  )
+  const navItems: NavItem[] = [
+    { href: '/diary', label: 'Diary', icon: null, additionalIcon: null },
+    { href: '/zip', label: 'ZIP', icon: null, additionalIcon: null },
+  ]
+
+  if (isLogin) {
+    navItems.push(
+      {
+        href: '/user',
+        label: '',
+        icon: <Icon name="User" width={18} height={20} fill={handleActive('/user', 'icon')} />,
+        additionalIcon: null,
+      },
+      {
+        href: '',
+        label: '',
+        icon: (
+          <Button
+            className="min-w-[188px] md:min-w-[55px] nav-item border-nav-border"
+            type="button"
+            aria-label="bell button"
+          >
+            <Icon name="Bell" width={18} height={18} />
+          </Button>
+        ),
+        additionalIcon: null,
+      }
+    )
+  }
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    const isMobile = window.matchMedia(`(max-width: 768px)`).matches
+
+    if (isMobile) {
+      e.preventDefault()
+
+      setIsActive(!isActive)
+    }
+  }
 
   return (
-    <header className="w-[100vw] h-[52px] flex-center fixed top-[10px] z-30">
-      <nav className="bg-nav-bg h-[100%] px-[10px] rounded-full flex-center">
-        <ul className="flex-center gap-[10px]">
-          {navList.map((item: NavItem) => (
-            <li key={item.index}>
-              {item.type === 'link' ? (
+    <header
+      className={`w-[100vw] ${isActive ? 'h-[55px]' : 'h-auto'} md:h-[52px] flex-center fixed top-[10px] z-30 overflow-hidden`}
+    >
+      <nav className="bg-nav-bg h-[100%] px-[10px] rounded-[28px] md:rounded-full md:flex-center">
+        <ul className="flex-center gap-[10px] flex-col md:flex-row mt-[8px] md:mt-[0px] mb-[8px] md:mb-[0px]">
+          <li>
+            <Link
+              className={`min-w-[188px] nav-item gap-[28px] ${handleActive('/', 'border')}`}
+              href="/"
+              onClick={handleClick}
+            >
+              <Icon name="Logo" width={35} height={35} className="mb-[10px]" />
+              <p className="text-[18px] font-bold">Moa</p>
+              <Icon
+                name="Home"
+                width={20}
+                height={20}
+                className="flex-center hidden md:block"
+                fill={handleActive('/', 'icon')}
+              />
+              <Icon
+                className="mt-[6px] block md:hidden"
+                name={isActive ? 'NavArrowTop' : 'NavArrowBottom'}
+                width={20}
+                height={13}
+              />
+            </Link>
+          </li>
+          <li className="block md:hidden">
+            <Link
+              href="/"
+              className={`min-w-[188px] nav-item gap-[28px] ${handleActive('/', 'border')}`}
+            >
+              <p className="text-[14px]">Home</p>
+            </Link>
+          </li>
+          {navItems.map((item) => (
+            <li key={item.href} className="min-w-[188px] md:min-w-[55px]">
+              {item.href ? (
                 <Link
-                  className={`min-w-[${item.minW}] nav-item gap-[28px] ${handleActive(item.index)}`}
-                  href={item.href || ''}
-                  onClick={() => handleClick(item.index)}
+                  href={item.href}
+                  className={`${item.href === '/user' ? 'min-w-[55px]' : 'min-w-[70px]'} nav-item gap-[28px] ${handleActive(item.href, 'border')}`}
                 >
-                  {item.script}
+                  {item.icon}
+                  {item.label && (
+                    <p className={`${item.href === '/' ? 'text-[18px] font-bold' : 'text-[14px]'}`}>
+                      {item.label}
+                    </p>
+                  )}
+                  {item.additionalIcon}
                 </Link>
               ) : (
-                <Button
-                  className={`nav-item ${handleActive(item.index)}`}
-                  onClick={() => handleClick(item.index)}
-                  style={{ minWidth: item.minW }}
-                  type="button"
-                  aria-label={item.icon?.name}
-                >
-                  {item.icon && (
-                    <Icon
-                      name={item.icon.name}
-                      width={item.icon.width}
-                      height={item.icon?.height}
-                      fill={tincolorIcon(activeIndex, item.index)}
-                    />
-                  )}
-                </Button>
+                item.icon
               )}
             </li>
           ))}
-          <ThemeToggle />
+          <li>
+            <Button
+              className="min-w-[188px] md:min-w-[55px] nav-item border-nav-border"
+              type="button"
+              aria-label="search button"
+            >
+              <Icon name="Search" width={18} height={18} />
+            </Button>
+          </li>
+          {isLogin ? (
+            <li>
+              <Button
+                className="min-w-[188px] md:min-w-[55px] nav-item border-nav-border"
+                type="button"
+                aria-label="logout button"
+              >
+                <Icon name="Logout" width={20} height={18} />
+              </Button>
+            </li>
+          ) : (
+            <li>
+              <Link
+                href="/login"
+                className={`min-w-[188px] md:min-w-[55px] nav-item gap-[28px] ${handleActive('/login', 'border')}`}
+              >
+                <Icon name="Login" width={20} height={18} fill={handleActive('/login', 'icon')} />
+              </Link>
+            </li>
+          )}
+          <li>
+            <ThemeToggle />
+          </li>
         </ul>
       </nav>
     </header>
