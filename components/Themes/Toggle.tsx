b4b1f6e9e5a676214ef523a8e4e04e7a3a4f64c5
YuAhyeon <dkgus7123@naver.com>
2024-07-28 03:00:04 +0900

    design: themes 토글 수정 #12

--- components/Themes/Toggle.tsx
+++ components/Themes/Toggle.tsx
@@ -5,12 +5,12 @@ import { useTheme } from 'next-themes'
 import { ThemeButton } from '../Button/ThemeButton'
 import { Icon } from '../Icon'
 
-export default function ThemeToggle({ size }: { size: string }) {
+export default function ThemeToggle() {
   const { resolvedTheme, setTheme } = useTheme()
 
   return (
     <ThemeButton
-      className={`${size === 'min' ? 'min-w-[55px]' : 'min-w-[188px]'} nav-item border-nav-border text-icon-normal`}
+      className="min-w-[188px] md:min-w-[55px] nav-item border-nav-border text-icon-normal"
       onClick={() => (resolvedTheme === 'dark' ? setTheme('light') : setTheme('dark'))}
     >
       <Icon name="Themes" width={20} height={20} fill="rgb(166,166,166)" />
