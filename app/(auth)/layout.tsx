ecf5b06536144751324f48659dc65cdbedb08672
YuAhyeon <dkgus7123@naver.com>
2024-07-29 10:06:02 +0900

    design: auth 공용 레이아웃 추가 및 Tap 추가 #20

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import Tap from '@/components/Page/Auth/Tap'
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <div className="w-[100vw] h-[100vh] flex-center flex-col">
+      <Tap />
+      {children}
+    </div>
+  )
+}
