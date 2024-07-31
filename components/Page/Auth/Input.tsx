9a863844349e5fa6241b2870b868c04f0238dc07
YuAhyeon <dkgus7123@naver.com>
2024-07-31 23:49:43 +0900

    design: input 수정 #20

--- components/Page/Auth/Input.tsx
+++ components/Page/Auth/Input.tsx
@@ -1,7 +1,12 @@
-export default function Input() {
+type InputProps = {
+  value: string
+  changeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export default function Input({ value, changeHandler }: InputProps) {
   return (
     <>
-      <div className="max-w-[350px] w-[50%] flex-center rounded-full border border-border focus:border-accent shadow-sm hover:shadow-md overflow-hidden text-[14px] px-[10px] gap-[10px] mb-[40px]">
+      <div className="max-w-[350px] w-[80%] md:w-[50%] flex-center rounded-full border border-border focus:border-accent shadow-sm hover:shadow-md overflow-hidden text-[14px] px-[10px] gap-[10px] mb-[40px]">
         <label
           htmlFor="email"
           className="inline-block text-heading-text font-bold pl-[30px] py-[18px]"
@@ -12,10 +17,10 @@ export default function Input() {
           id="email"
           type="email"
           placeholder="이메일을 입력해 주세요"
-          className="inline-block focus:outline-none pl-[20px] py-[18px] flex-1"
+          className="inline-block focus:outline-none pl-[20px] py-[18px] flex-1 bg-transparent"
         />
       </div>
-      <div className="max-w-[350px] w-[50%] flex-center rounded-full border border-border focus:border-accent shadow-sm hover:shadow-md overflow-hidden text-[14px] px-[10px] gap-[10px]">
+      <div className="max-w-[350px] w-[80%] md:w-[50%] flex-center rounded-full border border-border focus:border-accent shadow-sm hover:shadow-md overflow-hidden text-[14px] px-[10px] gap-[10px]">
         <label
           htmlFor="passward"
           className="inline-block text-heading-text font-bold pl-[30px] py-[18px]"
@@ -26,7 +31,7 @@ export default function Input() {
           id="passward"
           type="passward"
           placeholder="비밀번호를 입력해 주세요"
-          className="inline-block focus:outline-none pl-[20px] py-[18px] flex-1"
+          className="inline-block focus:outline-none pl-[20px] py-[18px] flex-1 bg-transparent"
         />
       </div>
     </>
