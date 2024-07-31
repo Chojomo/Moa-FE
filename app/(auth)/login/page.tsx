49cbfd58e7451e1eca4e9400ade46781f2566090
YuAhyeon <dkgus7123@naver.com>
2024-07-31 23:50:14 +0900

    design: 로그인 유지 체크박스, submit 버튼 추가 #20

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,35 @@
+'use client'
+
+import { useState } from 'react'
+import Input from '@/components/Page/Auth/Input'
+import Button from '@/components/Button'
+
 export default function Login() {
+  const [isChecked, setIsChecked] = useState(false)
+
+  /* input 따로따로 할 건지 고민,,, */
+  /* <Input /> */
+  /* 체크박스 아이콘으로 변경, submit 함수 추가할것... */
+
   return (
-    <div>
-      <p>Login</p>
-      <p>Login</p>
-      <p>Login</p>
-    </div>
+    <form>
+      <label className="flex items-center gap-[20px]">
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={() => setIsChecked(!isChecked)}
+          className="rounded-full h-5 w-5 text-blue-600"
+        />
+        <span className="text-heading-text text-[14px]">로그인 상태 유지하기</span>
+      </label>
+      <p>auth 아이콘</p>
+      <Button
+        type="submit"
+        ariaLabel="submit button"
+        className="bg-main-blue px-[130px] py-[10px] rounded-full font-bold text-[18px]"
+      >
+        회원가입
+      </Button>
+    </form>
   )
 }
