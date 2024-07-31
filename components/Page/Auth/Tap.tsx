624008d23dba045c186261cc698e68d5e4e68e5a
YuAhyeon <dkgus7123@naver.com>
2024-07-31 23:49:24 +0900

    design: Tap 순서 변경 #20

--- components/Page/Auth/Tap.tsx
+++ components/Page/Auth/Tap.tsx
@@ -9,16 +9,16 @@ export default function Tap() {
   return (
     <div className="h-auto text-[16px] font-bold border border-border rounded-full shadow-sm mb-[70px]">
       <Link
-        href="/signup"
-        className={`inline-block ${pathname === '/signup' && 'text-accent'} px-[50px] py-[10px] border-r border-r-border`}
-      >
-        회원가입
-      </Link>
-      <Link
         href="/login"
-        className={`inline-block ${pathname === '/login' && 'text-accent'} px-[50px] py-[10px]`}
+        className={`inline-block ${pathname === '/login' && 'text-accent'} px-[50px] py-[10px] border-r border-r-border`}
       >
         로그인
+      </Link>
+      <Link
+        href="/signup"
+        className={`inline-block ${pathname === '/signup' && 'text-accent'} px-[50px] py-[10px]`}
+      >
+        회원가입
       </Link>
     </div>
   )
