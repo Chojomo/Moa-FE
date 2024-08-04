8aa89c5110de327447fce06dfb743c3f6c7db860
YuAhyeon <dkgus7123@naver.com>
2024-08-04 22:13:19 +0900

    design: 로그인 input 컴포넌트 #20

--- components/Page/Auth/Input.tsx
+++ components/Page/Auth/Input.tsx
@@ -1,39 +1,30 @@
-// type InputProps = {
-//   value: string
-//   changeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void
-// }
+type InputProps = {
+  label: string
+  type: string
+  placeholder: string
+  value: string
+  changeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
 
-export default function Input() {
+export default function Input(props: InputProps) {
+  const { label, type, placeholder, value, changeHandler } = props
+
   return (
-    <>
-      <div className="max-w-[350px] w-[80%] md:w-[50%] flex-center rounded-full border border-border focus:border-accent shadow-sm hover:shadow-md overflow-hidden text-[14px] px-[10px] gap-[10px] mb-[40px]">
-        <label
-          htmlFor="email"
-          className="inline-block text-heading-text font-bold pl-[30px] py-[18px]"
-        >
-          이메일
-        </label>
-        <input
-          id="email"
-          type="email"
-          placeholder="이메일을 입력해 주세요"
-          className="inline-block focus:outline-none pl-[20px] py-[18px] flex-1 bg-transparent"
-        />
-      </div>
-      <div className="max-w-[350px] w-[80%] md:w-[50%] flex-center rounded-full border border-border focus:border-accent shadow-sm hover:shadow-md overflow-hidden text-[14px] px-[10px] gap-[10px]">
-        <label
-          htmlFor="passward"
-          className="inline-block text-heading-text font-bold pl-[30px] py-[18px]"
-        >
-          비밀번호
-        </label>
-        <input
-          id="passward"
-          type="passward"
-          placeholder="비밀번호를 입력해 주세요"
-          className="inline-block focus:outline-none pl-[20px] py-[18px] flex-1 bg-transparent"
-        />
-      </div>
-    </>
+    <div className="max-w-[350px] w-[80%] md:w-[50%] flex-center rounded-full border border-border focus:border-accent shadow-sm hover:shadow-md overflow-hidden text-[14px] px-[10px] gap-[10px] mb-[40px]">
+      <label
+        htmlFor={type}
+        className="inline-block text-heading-text font-bold pl-[30px] py-[18px]"
+      >
+        {label}
+      </label>
+      <input
+        id={type}
+        type={type}
+        placeholder={placeholder}
+        className="inline-block focus:outline-none pl-[20px] py-[18px] flex-1 bg-transparent"
+        value={value}
+        onChange={changeHandler}
+      />
+    </div>
   )
 }
