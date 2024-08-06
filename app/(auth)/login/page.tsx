3f5b8605e2c41335d00040c06b670d8557f11617
YuAhyeon <dkgus7123@naver.com>
2024-08-06 23:44:46 +0900

    feat: 보기/숨기기 및 입력 취소 추가 #21

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,14 +4,21 @@ import { useState } from 'react'
 import Button from '@/components/Button'
 import Input from '@/components/Page/Auth/Input'
 import { Icon } from '@/components/Icon'
+import { validateEmail } from '@/helper/validate'
 
 export default function Login() {
   const [emailValue, setEmailValue] = useState<string>('')
   const [passwordValue, setPasswordValue] = useState<string>('')
   const [isChecked, setIsChecked] = useState<boolean>(false)
+  const [isValidEmail, setIsValidEmail] = useState<boolean>(false)
+  const [showPassword, setShowPassword] = useState<boolean>(false)
 
   const changeEmailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmailValue(e.target.value)
+    const { value } = e.target
+    const isValid = validateEmail(value)
+
+    setEmailValue(value)
+    setIsValidEmail(isValid)
   }
 
   const changePasswordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,30 +26,60 @@ export default function Login() {
   }
 
   return (
-    <form className="flex-center flex-col">
-      <div className="w-[100vw] flex-center flex-col gap-[40px]">
-        <Input
-          label="이메일"
-          type="email"
-          placeholder="이메일을 입력해 주세요"
-          value={emailValue}
-          changeHandler={changeEmailHandler}
-        />
-        <Input
-          label="비밀번호"
-          type="password"
-          placeholder="비밀번호를 입력해 주세요"
-          value={passwordValue}
-          changeHandler={changePasswordHandler}
-        />
-      </div>
-      <div className="flex-center gap-[20px] my-[50px]">
+    <form className="w-[100vw] flex-center flex-col">
+      <Input
+        label="이메일"
+        type="email"
+        placeholder="이메일을 입력해 주세요"
+        value={emailValue}
+        changeHandler={changeEmailHandler}
+      >
+        <Button
+          type="button"
+          ariaLabel="Cancel email entry"
+          onClick={() => setEmailValue('')}
+          className={`absolute right-[15px] p-[5px] transition-opacity duration-300 ease-in-out ${emailValue ? 'opacity-100' : 'opacity-0'}`}
+        >
+          <Icon name="Cancel" width={15} height={15} />
+        </Button>
+      </Input>
+      <p className="relative bottom-[20px] right-[90px] text-[12px] font-bold text-accent">
+        유효하지 않은 이메일입니다.
+      </p>
+      <Input
+        label="비밀번호"
+        type={showPassword ? 'text' : 'password'}
+        placeholder="비밀번호를 입력해 주세요"
+        value={passwordValue}
+        changeHandler={changePasswordHandler}
+      >
+        <Button
+          type="button"
+          ariaLabel="Cancel email entry"
+          onClick={() => setPasswordValue('')}
+          className={`absolute right-[50px] p-[5px] transition-opacity duration-300 ease-in-out ${passwordValue ? 'opacity-100' : 'opacity-0'}`}
+        >
+          <Icon name="Cancel" width={15} height={15} />
+        </Button>
+        <Button
+          type="button"
+          ariaLabel="Toggle password visibility"
+          onClick={() => setShowPassword(!showPassword)}
+          className={`absolute right-[15px] p-[5px] transition-opacity duration-300 ease-in-out ${passwordValue ? 'opacity-100' : 'opacity-0'}`}
+        >
+          <Icon name={showPassword ? 'EyeOn' : 'EyeOff'} width={23} height={23} />
+        </Button>
+      </Input>
+      <p className="relative bottom-[20px] left-[5px] text-[12px] font-bold text-accent">
+        영어 대문자, 소문자, 특수문자를 포함한 8글자 이상을 입력해 주세요.
+      </p>
+      <div className="flex-center gap-[20px] mt-[30px]">
         <Button type="button" ariaLabel="check button" onClick={() => setIsChecked(!isChecked)}>
           <Icon name="Check" width={25} height={25} fill={isChecked ? '#80B0A2' : '#A6A6A6'} />
         </Button>
         <span className="text-heading-text text-[14px]">로그인 상태 유지하기</span>
       </div>
-      <div className="flex-center gap-[50px]">
+      <div className="flex-center gap-[50px] mt-[50px]">
         <Button type="button" ariaLabel="kakao auth button">
           <Icon name="Kakao" width={60} height={60} />
         </Button>
