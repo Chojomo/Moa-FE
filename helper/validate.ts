df66151f8cadb441bdd0b4f932828a75312f658d
YuAhyeon <dkgus7123@naver.com>
2024-08-07 00:09:39 +0900

    feat: 유효성 검사 로직 추가 #21

--- helper/validate.ts
+++ helper/validate.ts
@@ -1,5 +1,14 @@
 export const validateEmail = (email: string) => {
-  const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
-
+  const regExp = /^[0-9a-zA-Z]+@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
   return regExp.test(email)
+}
+
+export const validatePassword = (password: string) => {
+  const regExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#.~_-])[A-Za-z\d@$!%*?&#.~_-]{8,20}$/
+  return regExp.test(password)
+}
+
+export const validateNickname = (nickname: string) => {
+  const regExp = /^([a-zA-Z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣]).{2,10}$/
+  return regExp.test(nickname)
 }
