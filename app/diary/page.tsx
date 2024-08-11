0e5903426f452877a69178194ca27e884ff68e7c
YuAhyeon <dkgus7123@naver.com>
2024-08-11 23:05:04 +0900

    design: 다이어리 페이지 생성 #22

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -1,9 +1,18 @@
+import Pages from '@/components/Page/Diary'
+import Arrow from '@/components/Arrow'
+
 export default function Diary() {
   return (
-    <div>
-      <p>Diary</p>
-      <p>Diary</p>
-      <p>Diary</p>
+    <div
+      id="scroller"
+      className="relative bg-background w-[100vw] h-[100vh] overflow-x-hidden scroll-smooth"
+    >
+      <div className="h-[200vh]">
+        {Pages.map(({ name, Component }) => (
+          <Component key={name} />
+        ))}
+        <Arrow />
+      </div>
     </div>
   )
 }
