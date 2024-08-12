b7d7f64e92109e400987ad0f16a6c9e6566f6108
YuAhyeon <dkgus7123@naver.com>
2024-08-12 20:07:52 +0900

    update: 임시 post 상수 추가 #22

---
+++ helper/constants/popular.ts
@@ -0,0 +1,23 @@
+export const posts = [
+  {
+    index: 0,
+    src: '/images/octopus.jpeg',
+    alt: 'octopus',
+    title: '문어지지 않아 ~~',
+    description: '문어? 타코야키 좋아하세요?',
+  },
+  {
+    index: 1,
+    src: '/images/moon.jpeg',
+    alt: 'moon',
+    title: '문어문어?',
+    description: '문어? 타코야키 좋아하세요?',
+  },
+  {
+    index: 2,
+    src: '/images/moon.jpeg',
+    alt: '',
+    title: '점심을 먹으면 졸려',
+    description: '왜 졸려 어째서!',
+  },
+]
