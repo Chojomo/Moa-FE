2a7b1b355a7d6702e83549d1383a618241b64e6a
YuAhyeon <dkgus7123@naver.com>
2024-08-12 20:06:41 +0900

    design: popularCard 추가 #22

---
+++ components/Page/Diary/Intro/PopularCard.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+
+type Post = {
+  index: number
+  src: string | null
+  alt: string | null
+  title: string
+  description: string
+}
+
+type PopularCardProps = {
+  post: Post
+}
+
+export default function PopularCard({ post }: PopularCardProps) {
+  const { index, src, alt, title, description } = post
+
+  const addClass = () => {
+    switch (index) {
+      case 0:
+        return 'relative -rotate-8 top-[40px] left-[100px] animate-resetL'
+      case 1:
+        return 'z-10'
+      case 2:
+        return 'relative rotate-8 top-[40px] right-[100px] animate-resetR'
+      default:
+        return ''
+    }
+  }
+
+  return (
+    <div className={`text-center ${addClass()}`}>
+      <Image
+        src={src || '/images/moon.jpeg'}
+        width={0}
+        height={0}
+        alt={alt || 'popular post image'}
+        sizes="100vw"
+        className="w-[200px] h-[215px] rounded-lg mb-[20px] border border-border"
+      />
+      <h3 className="text-[18px] text-heading-text mb-[13px]">{title}</h3>
+      <p className="text-[12px] text-body-text">{description}</p>
+    </div>
+  )
+}
