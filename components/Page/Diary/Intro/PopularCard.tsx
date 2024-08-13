576e54c7131ea3aa780fa384f96f0281a126a4c6
YuAhyeon <dkgus7123@naver.com>
2024-08-13 22:53:27 +0900

    design: popularCard 모바일 #22

--- components/Page/Diary/Intro/PopularCard.tsx
+++ components/Page/Diary/Intro/PopularCard.tsx
@@ -29,16 +29,18 @@ export default function PopularCard({ post }: PopularCardProps) {
   }
 
   return (
-    <div className={`text-center ${addClass()}`}>
+    <div className={`flex-center flex-col ${addClass()}`}>
       <Image
         src={src || '/images/moon.jpeg'}
         width={0}
         height={0}
         alt={alt || 'popular post image'}
         sizes="100vw"
-        className="w-[200px] h-[215px] rounded-lg mb-[20px] border border-border"
+        className="md:w-[200px] md:h-[215px] w-[90px] h-[95px] rounded-lg md:mb-[20px] mb-[10px] border border-border"
       />
-      <h3 className="text-[18px] text-heading-text mb-[13px]">{title}</h3>
+      <h3 className="md:text-[18px] text-[15px] text-heading-text md:mb-[13px] mb-[7px]">
+        {title}
+      </h3>
       <p className="text-[12px] text-body-text">{description}</p>
     </div>
   )
