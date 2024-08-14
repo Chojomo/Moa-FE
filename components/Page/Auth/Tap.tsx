168ca9175f9b927eca09097776fc4717c3c1a202
YuAhyeon <dkgus7123@naver.com>
2024-08-14 16:18:13 +0900

    design: Tap 여백 수정 #20

--- components/Page/Auth/Tap.tsx
+++ components/Page/Auth/Tap.tsx
@@ -7,7 +7,7 @@ export default function Tap() {
   const pathname = usePathname()
 
   return (
-    <div className="h-auto text-[16px] font-bold border border-border rounded-full shadow-sm mb-[70px]">
+    <div className="h-auto text-[16px] font-bold border border-border rounded-full shadow-sm mb-[50px]">
       <Link
         href="/login"
         className={`inline-block ${pathname === '/login' && 'text-accent'} px-[50px] py-[10px] border-r border-r-border`}
