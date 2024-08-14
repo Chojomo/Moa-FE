f965873c0c8600f5c4de2d54d53bb5482a7f0e62
YuAhyeon <dkgus7123@naver.com>
2024-08-14 17:40:51 +0900

    design: 디테일 페이지 구조 #22

---
+++ components/Page/Diary/Detail/Post.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Post() {
+  return (
+    <div>
+      {/* <Image></Image> */}
+      <p>작성자</p>
+      <p>제묵</p>
+      <p>내용</p>
+    </div>
+  )
+}
