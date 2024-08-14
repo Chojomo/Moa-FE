3d5ce88142b625019077aca88eb7516a04470c7d
YuAhyeon <dkgus7123@naver.com>
2024-08-14 18:23:05 +0900

    design: Post 추가 #22

--- components/Page/Diary/Detail/Post.tsx
+++ components/Page/Diary/Detail/Post.tsx
@@ -1,12 +1,46 @@
 import Image from 'next/image'
 
-export default function Post() {
+type PostItem = {
+  index: number
+  src: string
+  writer: string
+  title: string
+  description: string
+}
+
+type PostProps = {
+  post: PostItem
+}
+
+export default function Post({ post }: PostProps) {
+  const { index, src, writer, title, description } = post
+
+  const addClass = () => {
+    if (index % 2 === 0) {
+      return 'sm:self-start'
+    }
+
+    if (index % 2 === 1) {
+      return 'sm:self-end sm:flex-row-reverse'
+    }
+
+    return ''
+  }
+
   return (
-    <div>
-      {/* <Image></Image> */}
-      <p>작성자</p>
-      <p>제묵</p>
-      <p>내용</p>
+    <div className={`flex-center gap-[45px] ${addClass()}`}>
+      <Image
+        src={src}
+        alt="post image"
+        width={100}
+        height={100}
+        className="border border-border rounded-lg"
+      />
+      <div className={`flex flex-col flex-1 gap-[5px] ${index % 2 === 0 ? '' : 'sm:items-end'}`}>
+        <p className="text-[16px] text-accent">{writer}</p>
+        <p className="text-[16px] text-heading-text font-bold">{title}</p>
+        <p className="text-[12px] text-body-text">{description}</p>
+      </div>
     </div>
   )
 }
