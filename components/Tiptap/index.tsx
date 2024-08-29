664f47ca53e2ef2e1c4248bede7d4bf2b0e4bfe1
YuAhyeon <dkgus7123@naver.com>
2024-08-29 15:01:17 +0900

    update: html content parse 추가 #24

--- components/Tiptap/index.tsx
+++ components/Tiptap/index.tsx
@@ -5,14 +5,13 @@ import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import Link from '@tiptap/extension-link'
 import { Markdown } from 'tiptap-markdown'
-import ReactMarkdown from 'react-markdown'
-import rehypeSanitize from 'rehype-sanitize'
-
+import DOMPurify from 'dompurify'
+import parse from 'html-react-parser'
 import Toolbar from './Toolbar'
 
 export default function Tiptap() {
   const [title, setTitle] = useState('')
-  const [content, setContent] = useState('gg')
+  const [content, setContent] = useState('')
 
   const editor = useEditor({
     extensions: [
@@ -29,40 +28,39 @@ export default function Tiptap() {
   })
 
   useEffect(() => {
-    if (editor && content) {
-      console.log('Setting content:', content)
+    if (editor) {
+      console.log(content)
       editor.commands.setContent(content)
     }
   }, [editor, content])
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-
-    console.log('Title:', title)
-    console.log('Content:', content)
+    console.log(title)
+    console.log(content)
   }
 
   return (
     <div className="w-[100vw] h-[100vh] flex-center px-[2%] pt-[10%] pb-[2%] gap-[50px]">
       <form onSubmit={handleSubmit} className="w-[50%] h-[100%] flex-center flex-col">
-        <div className="flex flex-col w-[100%] h-[20%]">
+        <div className="flex flex-col w-[100%] h-[25%]">
           <input
             type="text"
             placeholder="제목을 입력하세요"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className="w-[100%] flex-grow text-[28px] px-[30px] py-[20px] rounded focus:outline-none focus:ring-0"
+            className="w-[100%] flex-grow text-[28px] px-[20px] py-[20px] rounded focus:outline-none focus:ring-0"
           />
           {editor && <Toolbar editor={editor} />}
         </div>
         <EditorContent
           editor={editor}
-          className="bg-nav-bg w-[100%] focus:outline-none focus:ring-0 flex-grow overflow-y-auto"
+          className="bg-nav-bg w-[100%] focus:outline-none focus:ring-0 flex-grow px-[0px] py-[10px] overflow-y-auto"
           onClick={() => editor?.commands.focus()}
         />
       </form>
-      <div className="w-[50%] h-[100%] overflow-y-auto p-4 bg-white">
-        <ReactMarkdown rehypePlugins={[rehypeSanitize]}>{content}</ReactMarkdown>
+      <div className="w-[50%] h-[100%] overflow-y-auto p-4 bg-white text-body-text editor-preview">
+        {parse(DOMPurify.sanitize(content))}
       </div>
     </div>
   )
