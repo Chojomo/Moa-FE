c2edae93942a6e619c58ba820c880eec8921d833
YuAhyeon <dkgus7123@naver.com>
2024-08-31 22:07:25 +0900

    design: 에디터 라이브 변경으로 인한 demo editor 페이지 추가 #24

---
+++ app/demo/page.tsx
@@ -0,0 +1,7 @@
+export default function Demo() {
+  return (
+    <div>
+      <h1>Demo Editor</h1>
+    </div>
+  )
+}
--- app/zip/page.tsx
+++ app/zip/page.tsx
@@ -1,9 +1,17 @@
+import Link from 'next/link'
+
 export default function Zip() {
   return (
-    <div>
+    <div className="w-[100vw] h-[100vh] flex flex-col">
       <p>Zip</p>
       <p>Zip</p>
       <p>Zip</p>
+      <Link
+        href="/demo"
+        className="text-center w-[150px] p-[20px] bg-accent rounded-full text-black font-bold"
+      >
+        Demo Editor
+      </Link>
     </div>
   )
 }
