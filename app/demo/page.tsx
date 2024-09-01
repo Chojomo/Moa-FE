0d63a9dcb0a1eb543a2267dc1dad03c71dab2770
YuAhyeon <dkgus7123@naver.com>
2024-09-01 22:18:06 +0900

    update: toast ul 로 에디터 #24

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,7 +1,21 @@
+'use client'
+
+import { useRef } from 'react'
+import dynamic from 'next/dynamic'
+import { Editor as ToastEditor } from '@toast-ui/react-editor'
+
+const Editor = dynamic(() => import('../../components/Editor/index'), {
+  ssr: false,
+})
+
 export default function Demo() {
+  const ref = useRef<ToastEditor | null>(null)
+
   return (
-    <div>
-      <h1>Demo Editor</h1>
+    <div className="w-[100vw] h-[100vh]">
+      <form className="w-[100%] h-[100%]">
+        <Editor content="" editorRef={ref} />
+      </form>
     </div>
   )
 }
---
+++ components/Editor/index.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import '@toast-ui/editor/dist/toastui-editor.css'
+import 'tui-color-picker/dist/tui-color-picker.css'
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css'
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax'
+import { Editor } from '@toast-ui/react-editor'
+
+type EditorProps = {
+  content: string
+  editorRef: React.MutableRefObject<Editor | null>
+}
+
+export default function PostEditor({ content = '', editorRef }: EditorProps) {
+  const toolbarItems = [
+    ['heading', 'bold', 'italic', 'strike'],
+    ['hr'],
+    ['ul', 'ol', 'task'],
+    ['table', 'link'],
+    ['image'],
+    ['code'],
+    ['scrollSync'],
+  ]
+
+  return (
+    <div className="pt-[100px]">
+      {editorRef && (
+        <Editor
+          ref={editorRef}
+          initialValue={content || ' '}
+          initialEditType="markdown"
+          previewStyle={window.innerWidth > 1000 ? 'vertical' : 'tab'}
+          hideModeSwitch
+          height="calc(100vh - 380px)"
+          theme=""
+          usageStatistics={false}
+          toolbarItems={toolbarItems}
+          useCommandShortcut
+          plugins={[colorSyntax]}
+        />
+      )}
+    </div>
+  )
+}
