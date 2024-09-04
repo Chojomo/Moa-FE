cc924dddcad34bef8fa46868f6c3d8ddfea88c6b
YuAhyeon <dkgus7123@naver.com>
2024-09-04 18:39:54 +0900

    feat: 에디터 추가 #24

---
+++ app/diary/post/page.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useRef, useState, useEffect } from 'react'
+
+import dynamic from 'next/dynamic'
+import { Editor as ToastEditor } from '@toast-ui/react-editor'
+import { PreviwMode } from '@/types'
+
+const Editor = dynamic(() => import('../../../components/Editor/index'), {
+  ssr: false,
+})
+
+export default function Post() {
+  const ref = useRef<ToastEditor | null>(null)
+  const [title, setTitle] = useState<string>('')
+  const [preview, setPriview] = useState<PreviwMode>(window.innerWidth > 1000 ? 'vertical' : 'tab')
+
+  const handleResize = () => {
+    setPriview(window.innerWidth > 1000 ? 'vertical' : 'tab')
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return (
+    <div className="w-[100vw] h-[100vh]">
+      <form className="w-[100%] h-[100%]">
+        <input
+          type="text"
+          placeholder="제목을 입력하세요"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="w-[100%] text-[28px] px-[38px] pt-[60px] pb-[30px] rounded focus:outline-none focus:ring-0"
+        />
+        <Editor content="" editorRef={ref} preview={preview} />
+      </form>
+    </div>
+  )
+}
