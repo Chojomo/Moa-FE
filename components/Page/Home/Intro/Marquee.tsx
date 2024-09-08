308fb21604a172ec4c8cd877fb1908a9f29a8ad6
YuAhyeon <dkgus7123@naver.com>
2024-09-08 19:37:15 +0900

    design: 모바일 폰트 크기 수정 #25

--- components/Page/Home/Intro/Marquee.tsx
+++ components/Page/Home/Intro/Marquee.tsx
@@ -1,7 +1,7 @@
 export default function Marquee() {
   return (
     <div className="w-[100vw] absolute overflow-hidden">
-      <div className="whitespace-nowrap will-change-transform animate-slideLeft text-heading-bg text-[200px] font-bold ">
+      <div className="whitespace-nowrap will-change-transform animate-slideLeft text-heading-bg text-[100px] md:text-[200px] font-bold ">
         <span className="inline-block">
           &nbsp;JOIN US MOA! JOIN US MOA! JOIN US MOA! JOIN US MOA! JOIN US MOA! JOIN US MOA! JOIN
           US MOA!
