245b05ff61099bf86e862f90416ad522d9b1e203
YuAhyeon <dkgus7123@naver.com>
2024-09-18 17:39:07 +0900

    feat: 로그인 구현 #21

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,16 +1,21 @@
 'use client'
 
-import { useState, useCallback } from 'react'
-import Button from '@/components/Button'
-import OAuth from '@/components/Page/Auth/OAuth'
+import { useState, useCallback, FormEvent } from 'react'
+import { useRouter } from 'next/navigation'
+import { useMutation } from '@tanstack/react-query'
 import { validateEmail, validatePassword } from '@/helper/validate'
+import { login } from '@/lib/api/auth'
+import { useAuthStore } from '@/store/useAuth'
 
 import EmailInput from '@/components/Page/Auth/EmailInput'
 import PasswordInput from '@/components/Page/Auth/PasswordInput'
 import KeepSignedInCheckbox from '@/components/Page/Auth/KeepSignedInCheckbox'
 import SubmitButton from '@/components/Page/Auth/SubmitButton'
+import OAuth from '@/components/Page/Auth/OAuth'
 
 export default function Login() {
+  const router = useRouter()
+  const { login: setLogin } = useAuthStore()
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const [isChecked, setIsChecked] = useState<boolean>(false)
@@ -33,8 +38,29 @@ export default function Login() {
     setIsVisiblePassword(isValid)
   }, [])
 
+  const mutation = useMutation({
+    mutationFn: () => login(email, password),
+    onSuccess: (data) => {
+      console.log('성공!: ', data)
+      setLogin()
+      router.push('/')
+    },
+    onError: (error: unknown) => {
+      if (error instanceof Error) {
+        console.error('회원가입 실패:', error.message)
+      } else {
+        console.error('회원가입 실패:', error)
+      }
+    },
+  })
+
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    mutation.mutate()
+  }
+
   return (
-    <form className="w-[100vw] flex-center flex-col">
+    <form className="w-[100vw] flex-center flex-col" onSubmit={handleLogin}>
       <EmailInput email={email} handleChange={handleEmailChange} handleReset={() => setEmail('')} />
       <PasswordInput
         password={password}
--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -14,7 +14,30 @@ export const signup = async (userEmail: string, userPassword: string) => {
     if (!response.ok) {
       throw new Error(data.error || '회원가입 실패')
     } else {
-      return data.massage
+      return data.message
+    }
+  } catch (error) {
+    throw new Error('next 서버 요청 중 에러 발생')
+  }
+}
+
+export const login = async (userEmail: string, userPassword: string) => {
+  try {
+    const apiUrl = 'api/login'
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userEmail, userPassword }),
+    })
+
+    const data = await response.json()
+
+    if (!response.ok) {
+      throw new Error(data.error || '회원가입 실패')
+    } else {
+      return data.message
     }
   } catch (error) {
     throw new Error('next 서버 요청 중 에러 발생')
