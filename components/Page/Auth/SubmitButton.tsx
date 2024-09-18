b70dd122ce4b327bdf30c5ccebcd91a31180b57c
YuAhyeon <dkgus7123@naver.com>
2024-09-18 16:51:23 +0900

    design: 폼 제출 버튼 #21

--- components/Page/Auth/SubmitButton.tsx
+++ components/Page/Auth/SubmitButton.tsx
@@ -1,15 +1,17 @@
 import Button from '@/components/Button'
 
 type SubmitButtonProps = {
-  isValidEmail?: boolean
-  isValidPassword?: boolean
+  type: '로그인' | '회원가입'
+  isValidEmail: boolean
+  isValidPassword: boolean
   isMatched?: boolean
 }
 
 export default function SubmitButton({
+  type,
   isValidEmail,
   isValidPassword,
-  isMatched,
+  isMatched = true,
 }: SubmitButtonProps) {
   return (
     <Button
@@ -18,7 +20,7 @@ export default function SubmitButton({
       className={`mt-[47px] px-[130px] py-[10px] rounded-full font-bolㅋd text-[18px] text-[#fff] ${!isValidEmail || !isValidPassword || !isMatched ? 'bg-gray-400 cursor-not-allowed' : 'bg-main-blue'}`}
       disabled={!isValidEmail && !isValidPassword && isMatched}
     >
-      회원가입
+      {type}
     </Button>
   )
 }
