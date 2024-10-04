1c15f44ddaa9d2a873a32025223cd29071079ff1
YuAhyeon <dkgus7123@naver.com>
2024-10-04 17:33:44 +0900

    design: auth 간격 수정 #20

--- components/Page/Auth/Tap.tsx
+++ components/Page/Auth/Tap.tsx
@@ -7,7 +7,7 @@ export default function Tap() {
   const pathname = usePathname()
 
   return (
-    <div className="h-auto text-[16px] border border-border rounded-full shadow-sm mb-[50px]">
+    <div className="h-auto text-[16px] border border-border rounded-full shadow-sm my-[50px]">
       <Link
         href="/login"
         className={`inline-block ${pathname === '/login' ? 'text-heading-text font-bold' : 'text-nonActive-text'} px-[50px] py-[10px] border-r border-r-border`}
