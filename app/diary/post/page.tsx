7704d541891de965a9b8734d55be13c218fb7abf
YuAhyeon <dkgus7123@naver.com>
2024-10-06 23:49:49 +0900

    update: 제출 시 제목, 콘텐츠 검증 및 게시 구현 #24

--- app/diary/post/page.tsx
+++ app/diary/post/page.tsx
@@ -2,12 +2,15 @@
 
 import dynamic from 'next/dynamic'
 import { useState, useEffect, useRef } from 'react'
-import { useInitDiary, useAutoSaveDiary } from '@/hooks/editor'
+import { useInitDiary, useAutoSaveDiary, usePostDiary } from '@/hooks/editor'
 
 import TitleInput from '@/components/Page/Diary/Post/TitleInput'
 import ActionBar from '@/components/Page/Diary/Post/ActionBar'
 import { PreviwMode } from '@/types'
 import PublishModal from '@/components/Page/Diary/Post/Editor/Modal/PublishModal'
+
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const Editor = dynamic(() => import('../../../components/Page/Diary/Post/Editor/index'), {
   ssr: false,
@@ -24,6 +27,7 @@ export default function Post() {
   const [isPublic, setIsPublic] = useState<boolean>(true)
 
   const { mutate: initDiary } = useInitDiary()
+  const { mutate: postDiary } = usePostDiary()
   const { mutate: autoSaveDiary } = useAutoSaveDiary()
 
   const handleResize = () => {
@@ -51,7 +55,7 @@ export default function Post() {
   const saveDiary = () => {
     autoSaveDiary({
       diaryTitle: title,
-      diaryContentse: content,
+      diaryContents: content,
       isDiaryPublic: false,
     })
   }
@@ -70,8 +74,28 @@ export default function Post() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+
+    if (!title.trim()) {
+      return toast.error('제목을 입력하세요!')
+    }
+
+    if (!content.trim()) {
+      return toast.error('내용을 입력하세요!')
+    }
+
+    postDiary({
+      diaryTitle: title,
+      diaryContents: content,
+      diaryThumbnail: thumbnail,
+      isDiaryPublic: isPublic,
+    })
+  }
+
   return (
     <div className="w-[100vw] h-[100vh] overflow-hidden">
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
       <form className="w-[100vw] h-[100vh] flex flex-col">
         <TitleInput value={title} onChange={(e) => setTitle(e.target.value)} />
         <Editor value={content} onChange={(v) => setContent(v || '')} preview={preview} />
@@ -83,6 +107,7 @@ export default function Post() {
           setIsPublic={setIsPublic}
           thumbnail={thumbnail}
           setThumbnail={setThumbnail}
+          handleSubmit={handleSubmit}
         />
       </form>
     </div>
