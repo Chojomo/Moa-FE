3f1544567876a322c6f1675908a08d6fd251003c
YuAhyeon <dkgus7123@naver.com>
2024-10-06 22:56:51 +0900

    update: 썸네일 이미지 반환 url moa next Image 도메인 허용 #24

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 const path = require('path')
 
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'moa.ichubtou.com',
+        port: '',
+        pathname: '/api/v1/diaries/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '39.115.81.249',
+        port: '9000',
+        pathname: '/moa/diaries/**',
+      },
+    ],
+  },
   webpack: (config, { isServer }) => {
     const newConfig = { ...config }
 
