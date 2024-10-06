8849289c3f50fab3af2b2de5bc514b0273ec8177
YuAhyeon <dkgus7123@naver.com>
2024-10-06 15:52:31 +0900

    feat: 다이어리 썸네일 api 요청 #24

--- lib/api/diary.ts
+++ lib/api/diary.ts
@@ -117,3 +117,40 @@ export const putAutoSave = async ({
     throw new Error('next 서버 요청 중 에러 발생')
   }
 }
+
+export const postThumbnail = async (image: File) => {
+  const apiUrl = '/api/diary/thumbnail'
+  const token = localStorage.getItem('authToken')
+  const diaryId = localStorage.getItem('diaryId')
+
+  if (!token) {
+    throw new Error('로그인 상태를 확인하세요.')
+  }
+
+  const formData = new FormData()
+  formData.append('image', image)
+
+  if (diaryId) {
+    formData.append('diaryId', diaryId)
+  }
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        Authorization: token,
+      },
+      body: formData,
+    })
+
+    const data = await response.json()
+
+    if (!response.ok) {
+      throw new Error(data.error || '다이어리 썸네일 업로드 실패')
+    }
+
+    return data
+  } catch (error) {
+    throw new Error('next 서버 요청 중 에러 발생')
+  }
+}
