3b831079a14ca8036f48f55a5c37047bf0536cfa
YuAhyeon <dkgus7123@naver.com>
2024-10-14 17:50:43 +0900

    design: about 탭 부분 일단 추가 #39

--- app/user/about/page.tsx
+++ app/user/about/page.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image'
+
 export default function About() {
   return (
-    <div>
-      <h2>About</h2>
+    <div className="w-full flex-center flex-col flex-grow p-10 gap-3">
+      <Image
+        src="/images/pebble/mint-pebble.png"
+        alt="mint-pebble image"
+        width={70}
+        height={77}
+        quality={75}
+        loading="lazy"
+        draggable="false"
+      />
+      <p className="text-body-text">소개글이 비어있어요!</p>
     </div>
   )
 }
