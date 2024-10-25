c09e03fe18c5bd0d5f3d031c6d6c920417f1a75c
YuAhyeon <dkgus7123@naver.com>
2024-10-25 11:51:17 +0900

    design: 프로그래스 시작 #50

--- components/Page/Diary/Banner/index.tsx
+++ components/Page/Diary/Banner/index.tsx
@@ -1,3 +1,5 @@
+import Progress from './Progress'
+
 export default function Banner() {
   return (
     <section className="w-full h-[270px] flex-center gap-[10%] bg-banner-bg py-[40px]">
@@ -6,7 +8,7 @@ export default function Banner() {
           <h3 className="text-white text-[24px] font-bold mb-[18px]">인기 게시물</h3>
           <p className="text-[#FFFFFFCC] text-[16px]">현재 최고 인기 있는 게시물을 살펴 보세요</p>
         </div>
-        <div>progress</div>
+        <Progress />
       </div>
       <div className="text-[#FFFFFFCC]">diary post</div>
       {/* <div>arrow</div> */}
