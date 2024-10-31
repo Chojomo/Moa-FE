468fb135baaa9e882a9c5bb807e789f07d77396f
YuAhyeon <dkgus7123@naver.com>
2024-10-31 22:09:02 +0900

    design: Posts 추가 #50

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -1,11 +1,11 @@
-'use client'
-
 import Banner from '@/components/Page/Diary/Banner'
+import Posts from '@/components/Page/Diary/Posts'
 
 export default function Diary() {
   return (
-    <div className="w-[100vw] h-[100vh] pt-[95px]">
+    <div className="w-[100vw] h-[100vh] flex flex-col pt-[95px]">
       <Banner />
+      <Posts />
     </div>
   )
 }
