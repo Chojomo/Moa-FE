3e755e1b7cabdda451d362a1063dc52de9622921
YuAhyeon <dkgus7123@naver.com>
2024-11-03 14:17:35 +0900

    design: 임의 데이터로 연결 #50

--- components/Page/Diary/Posts/index.tsx
+++ components/Page/Diary/Posts/index.tsx
@@ -1,17 +1,15 @@
 import Sort from '../Sort'
+import Post from './Post'
+import { posts } from '@/helper/constants/posts'
 
 export default function Posts() {
   return (
     <div className="w-full flex-grow flex flex-col bg-background">
       <Sort />
-      <div className="w-[full] flex-grow">
-        <h1 className="text-[70px]">post</h1>
-        <h1 className="text-[70px]">post</h1>
-        <h1 className="text-[70px]">post</h1>
-        <h1 className="text-[70px]">post</h1>
-        <h1 className="text-[70px]">post</h1>
-        <h1 className="text-[70px]">post</h1>
-        <h1 className="text-[70px]">post</h1>
+      <div className="w-[full] flex flex-col flex-grow px-[10%] gap-[40px] sm:gap-[0px] mb-[95px]">
+        {posts.map((post) => (
+          <Post key={post.index} post={post} />
+        ))}
       </div>
     </div>
   )
