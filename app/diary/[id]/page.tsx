215ad94bedb1d0e1afe8a93c8d447ac017b91215
YuAhyeon <dkgus7123@naver.com>
2024-11-07 22:16:37 +0900

    design: 디테일 페이지 구조 잡기 #24

--- app/diary/[id]/page.tsx
+++ app/diary/[id]/page.tsx
@@ -4,10 +4,13 @@ type Params = {
 
 export default function DiaryDetail({ params }: { params: Params }) {
   return (
-    <div>
-      <p>{params.id}</p>
-      <p>{params.id}</p>
-      <p>{params.id}</p>
+    <div className="w-[100vw] h-[100vh] bg-teal-500 flex flex-col pt-[95px] overflow-auto">
+      <div className="bg-amber-300">타이틀/프로필/팔로우</div>
+      <div className="bg-amber-900">본문</div>
+      <div className="bg-violet-800">댓글입력</div>
+      <div className="bg-fuchsia-400">댓글목록</div>
+      <div className="bg-orange-500">하단 고정</div>
+      {/* <p>{params.id}</p> */}
     </div>
   )
 }
