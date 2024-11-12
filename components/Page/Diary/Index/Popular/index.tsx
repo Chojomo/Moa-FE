a3e9c970aadb56a860871621668e62ee76843afb
YuAhyeon <dkgus7123@naver.com>
2024-11-12 23:49:35 +0900

    feat: 조회순 다이어리 게시물 가져와 배너 적용 #23

--- components/Page/Diary/Index/Popular/index.tsx
+++ components/Page/Diary/Index/Popular/index.tsx
@@ -1,39 +1,64 @@
+'use client'
+
 import Image from 'next/image'
 import { Icon } from '@/components/Icon'
+import { Diary } from '@/types/diary'
 
-export default function PopularPost() {
+type PopularPostProps = {
+  post: Diary
+}
+
+export default function PopularPost({ post }: PopularPostProps) {
+  const {
+    diaryAuthorNickname,
+    diaryContents,
+    diaryPublishedAt,
+    diaryThumbnail,
+    diaryTitle,
+    totalLikes,
+  } = post
+
+  console.log(post)
+
+  let content = diaryContents.replace(/!\[Image\][^]*?-->/, '')
+
+  if (content.length > 100) {
+    content = content.substring(0, 30)
+  }
+
   return (
     <div className="relative w-[250px] h-[200px] rounded-[10px] overflow-hidden">
-      <Image
-        src="/images/ewtq.jpeg"
-        alt="popular post"
-        width={250}
-        height={200}
-        quality={75}
-        loading="lazy"
-        draggable="false"
-        objectFit="cover"
-      />
+      <div className="w-[250px] h-[200] transition-all animate-fadeIn">
+        <Image
+          src={diaryThumbnail || '/images/ewtq.jpeg'}
+          alt="popular post"
+          quality={75}
+          layout="fill"
+          objectFit="cover"
+          loading="lazy"
+          draggable="false"
+        />
+      </div>
       <div className="absolute left-0 bottom-0 w-full h-[70%] hover:h-[100%] transition-height duration-500 ease-in-out bg-[#00000099] flex flex-col text-white pt-[13px] pb-[9px] px-[13px]">
         <div className="flex-grow flex flex-col">
-          <p className="text-[14px] font-bold mb-[7px]">핫도그 언제 마지막으로...</p>
-          <p className="text-[12px] text-[#FFFFFFB3] mb-[18px] flex-grow">
-            망망곰곰엉덩일흔드러바흔들흔들
-          </p>
-          <p className="self-end text-[8px] mb-[5px]">2024.02.25</p>
+          <p className="text-[14px] font-bold mb-[7px]">{diaryTitle}</p>
+          <p className="text-[12px] text-[#FFFFFFB3] mb-[18px] flex-grow">{content}</p>
+          <p className="self-end text-[8px] mb-[5px]">{diaryPublishedAt}</p>
         </div>
         <div className="flex justify-between">
           <div className="text-[10px] font-bold flex items-center gap-3">
             <div className="flex gap-2">
               <Icon name="Heart" width={15} height={15} />
-              <span>3</span>
+              <span>{totalLikes}</span>
             </div>
             <div className="flex gap-2">
               <Icon name="Comment" width={17} height={17} />
               <span>2</span>
             </div>
           </div>
-          <span className="text-[15px] font-bold text-main-blue text-shadow">ichubtou</span>
+          <span className="text-[15px] font-bold text-main-blue text-shadow">
+            {diaryAuthorNickname}
+          </span>
         </div>
       </div>
     </div>
