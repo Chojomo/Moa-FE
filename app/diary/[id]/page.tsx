8b832a2594dddcea56699f50ce35d639104dbdc4
YuAhyeon <dkgus7123@naver.com>
2024-11-19 21:15:17 +0900

    design: Comment 아이템 UI 추가 #34

--- app/diary/[id]/page.tsx
+++ app/diary/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { Head, Content, CommentPost } from '@/components/Page/Diary/Detail'
+import { Head, Content, CommentPost, Comments } from '@/components/Page/Diary/Detail'
 
 type Params = {
   id: string
@@ -6,13 +6,12 @@ type Params = {
 
 export default function DiaryDetail({ params }: { params: Params }) {
   return (
-    <div className="relative w-[100vw] h-[100vh] flex flex-col pt-[100px] md:pt-[140px] overflow-auto px-[5%] md:px-[20%]">
+    <div className="relative w-[100vw] h-[100vh] flex flex-col pt-[100px] md:pt-[140px] overflow-auto px-[5%] md:px-[20%] pb-[60px]">
       <Head />
       <Content />
       <CommentPost />
-      <div className="bg-violet-800">댓글입력</div>
-      <div className="bg-fuchsia-400">댓글목록</div>
-      <div className="bg-orange-500">하단 고정</div>
+      <Comments />
+      <div className="fixed left-0 bottom-0 w-full h-[60px] bg-orange-500">하단 고정</div>
       {/* <p>{params.id}</p> */}
     </div>
   )
--- components/Page/Diary/Detail/index.tsx
+++ components/Page/Diary/Detail/index.tsx
@@ -1,3 +1,4 @@
 export { default as Head } from './Head'
 export { default as Content } from './Content'
 export { default as CommentPost } from './CommentPost'
+export { default as Comments } from './Comments'
