69acc1f8c5ef05c87bfff3378ebd6857e59dc1fa
YuAhyeon <dkgus7123@naver.com>
2024-11-19 20:15:07 +0900

    design: contend UI 추가 #34

--- app/diary/[id]/page.tsx
+++ app/diary/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { Head } from '@/components/Page/Diary/Detail'
+import { Head, Content } from '@/components/Page/Diary/Detail'
 
 type Params = {
   id: string
@@ -8,7 +8,7 @@ export default function DiaryDetail({ params }: { params: Params }) {
   return (
     <div className="relative w-[100vw] h-[100vh] flex flex-col pt-[100px] md:pt-[140px] overflow-auto px-[5%] md:px-[20%]">
       <Head />
-      <div>본문</div>
+      <Content />
       <div className="bg-violet-800">댓글입력</div>
       <div className="bg-fuchsia-400">댓글목록</div>
       <div className="bg-orange-500">하단 고정</div>
--- components/Page/Diary/Detail/Head/Button/Follow.tsx
+++ components/Page/Diary/Detail/Head/Button/Follow.tsx
@@ -4,13 +4,13 @@ import Button from '@/components/Button'
 
 export default function FollowButton() {
   const handleButtonClick = () => {
-    console.log('?')
+    console.log('클릭')
   }
   return (
     <Button
       type="button"
       ariaLabel="follow button"
-      className="bg-soft-bg px-[13px] md:px-[15px] py-[7px] rounded-md text-[10px] md:text-[13px] text-white font-semibold mr-3"
+      className="bg-soft-bg px-[13px] md:px-[15px] py-[7px] rounded-md text-[10px] md:text-[13px] text-white font-semibold mr-3 self-end"
       onClick={handleButtonClick}
     >
       팔로우
