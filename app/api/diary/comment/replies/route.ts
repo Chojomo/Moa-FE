9f07e4fff60deae5e0e537898cd228ddbbdfd2f7
YuAhyeon <dkgus7123@naver.com>
2024-11-25 15:53:03 +0900

    feat: 대댓글 POST 로직 우선 추가 #70

---
+++ app/api/diary/comment/replies/route.ts
@@ -0,0 +1,52 @@
+import { NextRequest } from 'next/server'
+
+export async function POST(req: NextRequest) {
+  const { searchParams } = new URL(req.url)
+  const diaryId = searchParams.get('diaryId')
+  const commentId = searchParams.get('commentId')
+
+  const apiUrl = `${process.env.API_URL}/api/v1/diaries/${diaryId}/comments/${commentId}/replies`
+  const token = req.headers.get('authorization')
+  const { replyContents } = await req.json()
+
+  if (!token) {
+    throw new Error('인증 헤더가 없습니다.')
+  }
+
+  if (!diaryId) {
+    throw new Error('다이어리가 존재하지 않습니다.')
+  }
+
+  if (!commentId) {
+    throw new Error('댓글이 존재하지 않습니다.')
+  }
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+      body: JSON.stringify({
+        replyContents,
+      }),
+    })
+
+    const data = await response.json()
+
+    if (!response.ok) {
+      throw new Error(data.error || '다이어리 게시 실패')
+    }
+
+    return new Response(JSON.stringify({ message: '댓글이 게시되었습니다.' }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  } catch (error) {
+    console.error('댓글 게시 에러:', error)
+    return new Response(JSON.stringify({ error: '서버 에러 발생' }), { status: 500 })
+  }
+}
--- lib/api/comment.ts
+++ lib/api/comment.ts
@@ -39,3 +39,51 @@ export const postComment = async ({
     throw new Error('next 서버 요청 중 에러 발생')
   }
 }
+
+export const postReply = async ({
+  diaryId,
+  commentId,
+  replyContents = '',
+}: {
+  diaryId: string
+  commentId: string
+  replyContents: string
+}) => {
+  const apiUrl = `${process.env.NEXT_PUBLIC_NEXT_API_URL}/api/diary/comment/replies?diaryId=${diaryId}&commentId=${commentId}`
+  const token = localStorage.getItem('authToken')
+
+  if (!token) {
+    throw new Error('로그인 상태를 확인하세요.')
+  }
+
+  if (!diaryId) {
+    throw new Error('다이어리가 존재하지 않습니다.')
+  }
+
+  if (!commentId) {
+    throw new Error('댓글이 존재하지 않습니다.')
+  }
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+      body: JSON.stringify({
+        replyContents,
+      }),
+    })
+
+    const data = await response.json()
+
+    if (!response.ok) {
+      throw new Error(data.error || '댓글 게시 실패')
+    }
+
+    return data
+  } catch (error) {
+    throw new Error('next 서버 요청 중 에러 발생')
+  }
+}
