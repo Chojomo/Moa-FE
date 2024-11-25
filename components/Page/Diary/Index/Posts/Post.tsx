fc15c8884dbd19aaf7c6d82d804993e4a5023f6a
YuAhyeon <dkgus7123@naver.com>
2024-11-25 22:18:00 +0900

    update: 썸네일, 타이틀, 본문 클릭 시 게시물로 이동 #70

--- components/Page/Diary/Index/Posts/Post.tsx
+++ components/Page/Diary/Index/Posts/Post.tsx
@@ -2,6 +2,7 @@ import { Icon } from '@/components/Icon'
 import { Diary } from '@/types/diary'
 import Image from 'next/image'
 import Entry from '@/components/Entry'
+import Link from 'next/link'
 
 type PostProps = {
   post: Diary
@@ -9,7 +10,7 @@ type PostProps = {
 }
 
 export default function Post({ index, post }: PostProps) {
-  const { diaryAuthorNickname, diaryThumbnail, diaryTitle, diaryContents } = post
+  const { diaryId, diaryAuthorNickname, diaryThumbnail, diaryTitle, diaryContents } = post
   let content = diaryContents.replace(/!\[Image\][^]*?-->/, '')
 
   if (content.length > 100) {
@@ -32,7 +33,7 @@ export default function Post({ index, post }: PostProps) {
     <div className={`relative flex-center gap-[45px] animate-fadeIn ${addClass()}`}>
       <div className="relative flex-center group">
         <Entry size="small" />
-        <div className="w-[120px] h-[120px]">
+        <Link href={`/diary/${diaryId}`} className="w-[120px] h-[120px]">
           <Image
             src={diaryThumbnail || '/images/dfsfs.jpeg'}
             alt="post image"
@@ -43,19 +44,21 @@ export default function Post({ index, post }: PostProps) {
             draggable="false"
             className="border border-border rounded-lg"
           />
-        </div>
+        </Link>
       </div>
       <div
         className={`flex flex-col flex-grow gap-[5px] overflow-hidden ${index % 2 === 0 ? '' : 'sm:items-end'}`}
       >
         <p className="text-[14px] text-main-blue">{diaryAuthorNickname}</p>
-        <p className="text-[16px] text-heading-text font-bold mb-[5px]">{diaryTitle}</p>
-        <p
-          className={`text-[12px] text-body-text overflow-hidden  ${index % 2 === 0 ? '' : 'text-right'}`}
-        >
-          {content}
-        </p>
-        <div className="text-[10px] font-bold flex items-center gap-3">
+        <Link href={`/diary/${diaryId}`}>
+          <p className="text-[16px] text-heading-text font-bold mb-[5px]">{diaryTitle}</p>
+          <p
+            className={`text-[12px] text-body-text overflow-hidden  ${index % 2 === 0 ? '' : 'text-right'} mb-3`}
+          >
+            {content}
+          </p>
+        </Link>
+        <div className="text-[10px] font-bold flex items-center gap-3 pointer-events-none">
           <div className="flex gap-2">
             <Icon name="Heart" width={15} height={15} />
             <span>3</span>
