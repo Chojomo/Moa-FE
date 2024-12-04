abe4a4dd13ea53f34069130526b274dac47e8070
YuAhyeon <dkgus7123@naver.com>
2024-12-04 22:58:54 +0900

    update: 게시물 타입 분리 #70

--- app/diary/[id]/page.tsx
+++ app/diary/[id]/page.tsx
@@ -3,39 +3,10 @@
 import { useState, useEffect } from 'react'
 import { Head, Content, Like, CommentPost, Comments, Footer } from '@/components/Page/Diary/Detail'
 import { getDiaryDetail } from '@/lib/api/diary'
+import { Post } from '@/types/diary'
 
 type Params = {
   id: string
-}
-
-type Comment = {
-  commentAuthorId: string
-  commentContents: string
-  commentId: string
-  createdAt: string
-  diaryAuthorNickname: string
-  diaryAuthorProfileImage: string
-  isCommentOwner: boolean
-  isLiked: boolean
-  likeCount: number
-}
-
-type Post = {
-  comment: Comment[]
-  commentCount: number
-  diaryAuthorId: string
-  diaryAuthorNickname: string
-  diaryAuthorProfileImage: string
-  diaryContents: string
-  diaryId: string
-  diaryPublishedAt: string
-  diaryThumbnail: string
-  diaryTitle: string
-  isDiaryOwner: boolean
-  isDiaryPublic: boolean
-  isLiked: boolean
-  likeCount: number
-  viewCount: number
 }
 
 export default function DiaryDetail({ params }: { params: Params }) {
@@ -57,12 +28,7 @@ export default function DiaryDetail({ params }: { params: Params }) {
 
   return (
     <div className="relative w-[100vw] h-[100vh] flex flex-col pt-[100px] md:pt-[140px] overflow-auto px-[5%] md:px-[20%] pb-[60px]">
-      <Head
-        diaryId={post.diaryId}
-        title={post.diaryTitle}
-        profile={post.diaryAuthorProfileImage}
-        publishedAt={post.diaryPublishedAt}
-      />
+      <Head post={post} />
       <Content content={post.diaryContents} />
       <Like diaryId={post.diaryId} isLiked={post.isLiked} />
       <CommentPost diaryId={post.diaryId} profile={post.diaryAuthorProfileImage} />
