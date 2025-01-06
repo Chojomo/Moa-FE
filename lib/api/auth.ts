d2ab81c0ce18750f70562524fa5c2bc6b46fb1bd
YuAhyeon <dkgus7123@naver.com>
2025-01-06 22:34:48 +0900

    feat: 임시 비밀번호 api #87

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -49,3 +49,30 @@ export const login = async (userEmail: string, userPassword: string) => {
     throw new Error('next 서버 요청 중 에러 발생')
   }
 }
+
+export const findPassword = async (email: string) => {
+  if (!email) {
+    throw new Error('이메일 정보가 없습니다.')
+  }
+
+  try {
+    const apiUrl = 'api/signin/find/password'
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email }),
+    })
+
+    const data = await response.json()
+
+    if (!response.ok) {
+      throw new Error(data.error || '임시 비밀번호 발송 실피')
+    } else {
+      return data.data
+    }
+  } catch (e) {
+    throw new Error('next 서버 요청 중 에러 발생')
+  }
+}
