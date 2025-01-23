90ee5d57997c064e1cb9791a00dd67a3e23880fb
YuAhyeon <dkgus7123@naver.com>
2025-01-23 22:31:13 +0900

    design: 모달 색상 수정 #98

--- components/Page/SuikaGame/Modal/index.tsx
+++ components/Page/SuikaGame/Modal/index.tsx
@@ -20,22 +20,20 @@ export default function GameModal({ isOpen, score, handleClose, handleRestart }:
   return (
     <Modal
       isOpen={isOpen}
-      onRequestClose={handleClose}
+      // onRequestClose={handleClose}
       contentLabel="카테고리 모달"
-      className="max-w-[400px] w-[90%] md:w-[50%] h-[290px] flex flex-col justify-between items-center bg-background border border-border animate-fade rounded font-mono py-[35px]"
+      className="max-w-[400px] w-[90%] md:w-[50%] h-[290px] flex flex-col justify-between items-center bg-[#ffffffeb] animate-fade rounded-3xl font-mono py-[35px]"
       overlayClassName="modal-overlay-transparent"
     >
       <div className="text-center flex flex-col gap-5">
-        <p className="text-[1.1rem]">
-          나의 점수 : <span className="text-[1.2rem] text-main-blue font-semibold">{score}</span>
-        </p>
-        <p className=" text-[1.3rem]">아쉽네요! 다시 도전하시겠어요?</p>
+        <p className="text-[1.5rem] font-semibold text-[#6F4927]">{score}</p>
+        <p className=" text-[1.3rem] font-semibold">아쉽네요! 다시 도전하시겠어요?</p>
       </div>
       <div className="w-full flex flex-col justify-center items-center gap-4">
         <Button
           type="button"
           ariaLabel="다시 하기 버튼"
-          className="w-[80%] py-2 border rounded hover:bg-main-blue"
+          className="w-[80%] py-2 border-2 border-[#6F4927] rounded hover:bg-[#ffdcc84b] font-semibold"
           onClick={handleRestart}
         >
           다시 도전하기
@@ -43,7 +41,7 @@ export default function GameModal({ isOpen, score, handleClose, handleRestart }:
         <Button
           type="button"
           ariaLabel="공유하기 버튼"
-          className=" w-[80%] py-2 border rounded hover:bg-main-blue"
+          className=" w-[80%] py-2 border-2 border-[#6F4927] rounded hover:bg-[#ffdcc84b] font-semibold"
           onClick={handleShare}
         >
           공유하기
