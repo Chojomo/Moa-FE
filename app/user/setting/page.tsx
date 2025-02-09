198e5e3cf23abf70cd4659c604e8d3278dba2b9a
YuAhyeon <dkgus7123@naver.com>
2025-02-09 22:31:23 +0900

    remove: 불필요 import문 제거 #39

--- app/user/setting/page.tsx
+++ app/user/setting/page.tsx
@@ -1,9 +1,5 @@
 'use client'
 
-import { useState, useCallback, useRef } from 'react'
-import Image from 'next/image'
-import Button from '@/components/Button'
-
 import {
   Profile,
   Nickname,
@@ -14,19 +10,6 @@ import {
 } from '@/components/Page/User/Setting'
 
 export default function Setting() {
-  const fileInputRef = useRef<HTMLInputElement>(null)
-  const [profile, setProfile] = useState<null | string>(null)
-  // const [aboutText, setAboutText] = useState<string>('')
-
-  const handleChange = useCallback(
-    (setter: React.Dispatch<React.SetStateAction<string>>) =>
-      (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { value } = e.target
-        setter(value)
-      },
-    []
-  )
-
   return (
     <div className="relative w-full pt-[5%] flex flex-col pb-[5%] gap-[20px]">
       <Profile />
