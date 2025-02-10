15ec1ac22a75e0c0862569a777189372e536c873
YuAhyeon <dkgus7123@naver.com>
2025-02-10 10:01:04 +0900

    design: 탈퇴 모달 추가 #39

--- components/Page/User/Setting/DeleteAccount/index.tsx
+++ components/Page/User/Setting/DeleteAccount/index.tsx
@@ -1,17 +1,9 @@
 import Button from '@/components/Button'
-import { PasswordInput } from '@/components/Page/Auth/Input'
-
 import { useState } from 'react'
+import DeleteAccountModal from './Modal'
 
 export default function DeleteAccount() {
-  const [isEdit, setIsEdit] = useState<boolean>(false)
-
-  const handleCancleClick = () => {
-    setIsEdit(false)
-  }
-  const handleSaveClick = () => {
-    setIsEdit(true)
-  }
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
 
   return (
     <div className="w-full pt-[10px] pb-[40px] flex flex-col justify-center gap-4">
@@ -21,14 +13,15 @@ export default function DeleteAccount() {
           type="button"
           ariaLabel="닉네임 변경 확인 버튼"
           className="bg-[#d5d5d5] dark:bg-[#333333] hover:bg-[#d0d0d0] dark:hover:bg-[#2d2d2d] rounded-md p-2 text-[0.8rem]"
-          onClick={() => setIsEdit(true)}
+          onClick={() => setIsModalOpen(true)}
         >
           탈퇴
         </Button>
       </div>
       <p className="text-nonActive-text text-[0.8rem]">
-        유효한 이메일로 가입 시, 해당 이메일을 통해 비밀번호를 변경할 수 있습니다.
+        회원 탈퇴 시, 작성했던 글과 댓글은 유지되며, 수정 및 삭제할 수 없습니다.
       </p>
+      <DeleteAccountModal isOpen={isModalOpen} handleClose={() => setIsModalOpen(false)} />
     </div>
   )
 }
