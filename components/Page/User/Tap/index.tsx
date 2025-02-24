79c2866d721f5326a552c55f6126b9b57dc4bd14
YuAhyeon <dkgus7123@naver.com>
2025-02-24 17:33:17 +0900

    update: 탭 활성화 추가 #127

--- components/Page/User/Tap/index.tsx
+++ components/Page/User/Tap/index.tsx
@@ -6,17 +6,16 @@ import { usePathname } from 'next/navigation'
 
 export default function Tap() {
   const pathname = usePathname()
-
-  console.log(pathname)
+  const currentTap = pathname.split('/').at(-1)
 
   return (
     <div className="relative w-full h-[60px] mt-[50px] flex-center">
       <ul className="w-full flex items-start gap-6 text-[1.1rem] py-5 text-nonActive-text border-b border-border">
-        {TAPS.map(({ name, href, path }) => (
+        {TAPS.map(({ name, href }) => (
           <li key={name}>
             <Link
               href={href}
-              className={`${pathname === path && 'text-heading-text font-bold'} pr-5 py-5`}
+              className={`${currentTap === href && 'text-heading-text font-bold'} pr-5 py-5`}
             >
               {name}
             </Link>
