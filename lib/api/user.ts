5c23567ff536d9975226c6a526d1bc90ba8ec4c3
YuAhyeon <dkgus7123@naver.com>
2025-02-27 23:13:20 +0900

    feat: 유저 좋아요 다이어리 리스트 api get 요청 #127

--- lib/api/user.ts
+++ lib/api/user.ts
@@ -106,3 +106,39 @@ export const getUserComments = async ({
     throw new Error('next 서버 요청 중 에러 발생')
   }
 }
+
+export const getUserLikes = async ({
+  userId,
+  pageParam = 0,
+  pageSize = 10,
+}: {
+  userId: string
+  pageParam: number
+  pageSize: number
+}) => {
+  const apiUrl = `/api/user/likes?userId=${userId}&pageNumber=${pageParam}&pageSize=${pageSize}`
+
+  if (!userId) {
+    throw new Error('유저가 존재하지 않습니다.')
+  }
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    const data = await response.json()
+
+    if (!response.ok) {
+      console.log(data.error)
+      throw new Error(data.error || '유저 좋아요 다이어리 리스트 가져오기 실패')
+    }
+
+    return data
+  } catch (error) {
+    throw new Error('next 서버 요청 중 에러 발생')
+  }
+}
