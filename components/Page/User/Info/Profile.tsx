2b6789f793321717991482c80ba7cfeafd95aa36
YuAhyeon <dkgus7123@naver.com>
2025-02-28 15:59:08 +0900

    update: 유저 팔로우 리스트 데이터 props로 넘겨 주고 렌더링 #127

--- components/Page/User/Info/Profile.tsx
+++ components/Page/User/Info/Profile.tsx
@@ -1,12 +1,26 @@
+import { useState } from 'react'
 import Image from 'next/image'
+import Button from '@/components/Button'
 import { User } from '@/types/user'
 import { BASE_PROFILE } from '@/helper/constants'
+import ListModal from '@/components/UI/Modal/ListModal'
+
+type FollowInfo = {
+  userId: string
+  userNickname: string
+  userProfileImage: string
+}
 
 type ProfileProps = {
   user?: User | null
+  following: FollowInfo[]
+  followers: FollowInfo[]
 }
 
-export default function Profile({ user }: ProfileProps) {
+export default function Profile({ user, following, followers }: ProfileProps) {
+  const [activeModal, setActiveModal] = useState<'followers' | 'following' | null>(null)
+  const [isModalOpen, setIsModalOpen] = useState(false) // 모달이 열려 있는지 여부
+
   if (!user) {
     return <p>로딩 중</p>
   }
@@ -15,10 +29,20 @@ export default function Profile({ user }: ProfileProps) {
     userProfileImage: src,
     userNickname: name,
     userIntroduce: dio,
-    followingCount: follows,
-    followerCount: followers,
+    followingCount,
+    followerCount,
     isMyPage,
   } = user
+
+  const openModal = (type: 'followers' | 'following') => {
+    setActiveModal(type)
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setActiveModal(null)
+  }
 
   return (
     <div className="w-full flex flex-col gap-[20px]">
@@ -35,16 +59,32 @@ export default function Profile({ user }: ProfileProps) {
       </div>
       <p className="text-heading-text text-[2.5rem] font-serif font-semibold">{name}</p>
       <p>{dio}</p>
+
       {!isMyPage && (
         <div className="flex gap-7 pt-3 text-[0.9rem] text-nonActive-text">
-          <p>
-            <span className="text-heading-text font-semibold pr-3">{follows}</span> 팔로우
-          </p>
-          <p>
-            <span className="text-heading-text font-semibold pr-3">{followers}</span> 팔로워
-          </p>
+          <Button
+            type="button"
+            ariaLabel="팔로워 모달 열기 버튼"
+            onClick={() => openModal('followers')}
+          >
+            <span className="text-heading-text font-semibold pr-3">{followerCount}</span> 팔로워
+          </Button>
+          <Button
+            type="button"
+            ariaLabel="팔로잉 모달 열기 버튼"
+            onClick={() => openModal('following')}
+          >
+            <span className="text-heading-text font-semibold pr-3">{followingCount}</span> 팔로잉
+          </Button>
         </div>
       )}
+
+      <ListModal
+        isOpen={isModalOpen}
+        handleClose={closeModal}
+        list={activeModal === 'followers' ? followers : following}
+        title={activeModal === 'followers' ? '팔로워 목록' : '팔로잉 목록'}
+      />
     </div>
   )
 }
--- components/Page/User/Info/index.tsx
+++ components/Page/User/Info/index.tsx
@@ -5,44 +5,58 @@ import { Icon } from '@/components/Icon'
 
 import { useQuery } from '@tanstack/react-query'
 import { getUser } from '@/lib/api/user'
+import { useAuthStore } from '@/store/useAuth'
 
-import Button from '@/components/Button'
 import { User } from '@/types/user'
-import Profile from './Profile'
+import Button from '@/components/Button'
+import Profile from '@/components/Page/User/Info/Profile'
+
+type FollowInfo = {
+  userId: string
+  userNickname: string
+  userProfileImage: string
+}
 
 type UserInfoProps = {
   userId: string
-  initialData: User
+  userInitialData: User
+  following: FollowInfo[]
+  followers: FollowInfo[]
 }
 
-export default function UserInfo({ userId, initialData }: UserInfoProps) {
+export default function UserInfo({ userId, userInitialData, following, followers }: UserInfoProps) {
+  const { isLogin } = useAuthStore()
   const { data: user } = useQuery({
     queryKey: ['user'],
     queryFn: () => getUser({ userId }),
-    initialData,
+    initialData: userInitialData,
   })
 
   return (
     <div className="w-full relative">
-      <Profile user={user} />
-      {user?.isMyPage ? (
-        <Link href="/user/setting" className="absolute top-[10px] right-[20px] group p-2">
-          <Icon
-            name="Setting"
-            width={30}
-            height={30}
-            className="text-[#636363] group-hover:text-[#404040] dark:text-[#b2b2b2] dark:group-hover:text-[#e8e8e8] transition-colors"
-          />
-        </Link>
-      ) : (
-        <Button
-          type="button"
-          ariaLabel="팔로우 버튼"
-          className="absolute top-[20px] right-[20px] px-[18px] py-[5px] bg-main-blue hover:bg-[#3878d7] rounded-full text-white font-semibold"
-        >
-          팔로우
-        </Button>
-      )}
+      <Profile user={user} following={following} followers={followers} />
+      {isLogin &&
+        (user?.isMyPage ? (
+          <Link
+            href={`/user/${userId}/setting`}
+            className="absolute top-[10px] right-[20px] group p-2"
+          >
+            <Icon
+              name="Setting"
+              width={30}
+              height={30}
+              className="text-[#636363] group-hover:text-[#404040] dark:text-[#b2b2b2] dark:group-hover:text-[#e8e8e8] transition-colors"
+            />
+          </Link>
+        ) : (
+          <Button
+            type="button"
+            ariaLabel="팔로우 버튼"
+            className="absolute top-[20px] right-[20px] px-[18px] py-[5px] bg-main-blue hover:bg-[#3878d7] rounded-full text-white font-semibold"
+          >
+            {user?.isFollowing ? '언팔로우' : '팔로우'}
+          </Button>
+        ))}
     </div>
   )
 }
