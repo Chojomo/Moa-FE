e92f337932dde858aff4430b5ef208e1ec2606d1
YuAhyeon <dkgus7123@naver.com>
2025-03-19 17:27:29 +0900

    feat: 메시지 입력 자동 완성 제거 및 form으로 관리 #134

--- components/Page/Chat/ChatRoom/ChatRoomInput/index.tsx
+++ components/Page/Chat/ChatRoom/ChatRoomInput/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, Dispatch, SetStateAction } from 'react'
+import { useCallback, Dispatch, SetStateAction, FormEvent } from 'react'
 import Button from '@/components/Button'
 import { Icon } from '@/components/Icon'
 import { isTouchDevice } from '@/utils'
@@ -15,9 +15,18 @@ export default function ChatRoomInput({ message, setMessage }: ChatRoomInputProp
     setMessage(value)
   }, [])
 
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+
+    if (message.trim() !== '') {
+      setMessage('')
+    }
+  }
+
   return (
-    <div
+    <form
       className={`${isMobile ? 'w-full' : 'w-full max-w-[500px]'} h-[64px] fixed bottom-0 flex-center bg-background overflow-hidden`}
+      onSubmit={handleSubmit}
     >
       <div className="relative w-full h-full flex-center">
         <input
@@ -26,6 +35,9 @@ export default function ChatRoomInput({ message, setMessage }: ChatRoomInputProp
           name="message"
           aria-label="채팅 메시지 입력"
           placeholder="메시지 입력"
+          autoCorrect="off"
+          autoComplete="off"
+          spellCheck={false}
           value={message}
           onChange={handleMessageChange}
           className="input-reset w-[96%] h-[75%] autofull-text border border-border rounded-full pl-[20px] pr-[64px] text-[1rem] sm:text-[1.2rem]"
@@ -38,6 +50,6 @@ export default function ChatRoomInput({ message, setMessage }: ChatRoomInputProp
           <Icon name="ArrowUp" width={20} height={20} className="text-white" />
         </Button>
       </div>
-    </div>
+    </form>
   )
 }
