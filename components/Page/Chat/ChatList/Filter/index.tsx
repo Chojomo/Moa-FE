0a35b3281a00d6ef3d266ae25f3d86e136b1cf4e
YuAhyeon <dkgus7123@naver.com>
2025-03-19 15:38:39 +0900

    design: chat Item #131

--- components/Page/Chat/ChatList/Filter/index.tsx
+++ components/Page/Chat/ChatList/Filter/index.tsx
@@ -6,14 +6,14 @@ export default function Filter() {
       <Button
         type="button"
         ariaLabel="전체 채팅방 필터 버튼"
-        className="flex-center bg-icon-bg dark:bg-[#2f2f2f] px-3 py-2 font-bold text-white text-[0.8rem] rounded-full"
+        className="flex-center bg-[#2d2d2d] dark:bg-[#2f2f2f] px-3 py-2 font-bold text-white text-[0.8rem] rounded-full"
       >
         전체
       </Button>
       <Button
         type="button"
         ariaLabel="안 읽은 채팅방 필터 버튼"
-        className="flex-center bg-icon-bg dark:bg-[#5f5f5f] px-3 py-2 font-bold text-white text-[0.8rem] rounded-full"
+        className="flex-center bg-[#474747] dark:bg-[#5f5f5f] px-3 py-2 font-bold text-white text-[0.8rem] rounded-full"
       >
         안 읽은 채팅방
       </Button>
