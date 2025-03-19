fcec8d9f4e8f1926ed7287b6b7cc8eaa000298cf
YuAhyeon <dkgus7123@naver.com>
2025-03-19 15:39:23 +0900

    design: chat room #131

--- app/zip/chat/page.tsx
+++ app/zip/chat/page.tsx
@@ -1,17 +1,15 @@
 'use client'
 
-import { isTouchDevice } from '@/utils'
 import { Header, ChatList } from '@/components/Page/Chat'
 
 export default function Chat() {
-  const isMobile = isTouchDevice()
+  const userInfo = JSON.parse(localStorage.getItem('userInfo') || '{}')
+  const { userId, nickname } = userInfo
 
   return (
-    <div className="bg-background w-[100dvw] h-[100dvh] flex-center">
-      <div className={`${isMobile ? 'w-[100dvw]' : 'w-full max-w-[500px]'} h-full flex flex-col`}>
-        <Header />
-        <ChatList />
-      </div>
+    <div className="w-full">
+      <Header />
+      <ChatList />
     </div>
   )
 }
