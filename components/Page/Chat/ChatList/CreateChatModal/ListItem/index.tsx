87309740576941dfd7ce6fc4804b7deeb9625f25
YuAhyeon <dkgus7123@naver.com>
2025-05-16 22:31:24 +0900

    feat: 채팅 가능 팔로우 목록 추가 #134

---
+++ components/Page/Chat/ChatList/CreateChatModal/ListItem/index.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+type ListItemProps = {
+  userId: string
+  username: string
+  imgSrc: string
+}
+
+export default function listItem({ userId, username, imgSrc }: ListItemProps) {
+  return (
+    <Link href={`/zip/chat/${userId}`} className="flex items-center gap-5 animate-fadeIn">
+      <Image
+        src={imgSrc}
+        alt="user profile"
+        width={45}
+        height={45}
+        quality={75}
+        loading="lazy"
+        draggable="false"
+        objectFit="cover"
+        className="w-[45px] h-[45px] rounded-full border border-border"
+      />
+      <p className="text-[1rem] text-body-text">{username}</p>
+    </Link>
+  )
+}
