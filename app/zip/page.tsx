5cf5cb37b642bd61081a8dd6e67081a6f332b5ae
YuAhyeon <dkgus7123@naver.com>
2025-05-19 14:52:33 +0900

    update: 데모 에디터 제거

--- app/zip/page.tsx
+++ app/zip/page.tsx
@@ -1,17 +1,7 @@
-import Link from 'next/link'
-
 export default function Zip() {
   return (
     <div className="w-[100vw] h-[100vh] flex flex-col">
       <p>Zip</p>
-      <p>Zip</p>
-      <p>Zip</p>
-      <Link
-        href="/demo"
-        className="text-center w-[150px] p-[20px] bg-accent rounded-full text-black font-bold"
-      >
-        Demo Editor
-      </Link>
     </div>
   )
 }
