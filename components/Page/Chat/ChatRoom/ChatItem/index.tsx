d866d92c8298fa36630777a892de9538de6284f4
YuAhyeon <dkgus7123@naver.com>
2025-07-22 19:09:13 +0900

    update: 채팅 말풍선 추가 #134

--- components/Page/Chat/ChatRoom/ChatItem/index.tsx
+++ components/Page/Chat/ChatRoom/ChatItem/index.tsx
@@ -1,44 +1,51 @@
-import Image from 'next/image'
+'use client'
+
+import { useWebSocketStore } from '@/store/useSocket'
 
 type Message = {
-  userId: string
-  username: string
-  avatar: string
+  _id?: string
+  userId?: string
+  nickname: string
   message: string
+  roomId: string
   timestamp: string
+  __v?: number
 }
-
 type ChatItemProps = {
   message: Message
 }
 
 export default function ChatItem({ message }: ChatItemProps) {
-  const isSelf = message.userId === '822b934e-0439-4cf0-b9ae-d25a14756ffe'
+  const { userId } = useWebSocketStore()
+  const isSelf = message.userId === userId
+
+  const formatTimestamp = (iso: string): string => {
+    const date = new Date(iso)
+
+    const year = String(date.getFullYear()).slice(2)
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const day = String(date.getDate()).padStart(2, '0')
+    const hours = String(date.getHours()).padStart(2, '0')
+    const minutes = String(date.getMinutes()).padStart(2, '0')
+
+    return `${year}.${month}.${day} ${hours}:${minutes}`
+  }
+
   return (
-    <div
-      className={`flex items-start justify-start gap-[15px] ${isSelf ? 'flex-row-reverse' : ''}`}
-    >
-      {!isSelf && (
-        <Image
-          src={message.avatar}
-          alt="user profile"
-          width={50}
-          height={50}
-          quality={75}
-          loading="lazy"
-          draggable="false"
-          objectFit="cover"
-          className="w-[40px] h-[40px] sm:w-[50px] sm:h-[50px] rounded-full"
-        />
-      )}
-      <div className="bg-[#EAEBEE] dark:bg-[#2a2a2a] px-[10px] py-[10px]  sm:px-[15px] sm:py-[12px] rounded text-body-text">
-        {message.message}
+    <div className={`flex items-start justify-start gap-[15px] ${isSelf && 'flex-row-reverse'}`}>
+      <div className="flex flex-col gap-2">
+        {!isSelf && (
+          <p className="text-main-blue text-[1.1rem] font-semibold">{message.nickname}</p>
+        )}
+        <div className={`flex gap-2 ${isSelf ? 'flex-row-reverse' : ''}`}>
+          <div className="bg-[#EAEBEE] dark:bg-[#2a2a2a] px-[10px] py-[10px]  sm:px-[15px] sm:py-[10px] rounded text-body-text">
+            {message.message}
+          </div>
+          <p className="text-[0.7rem] self-end text-[#A1A4A9]">
+            {formatTimestamp(message.timestamp)}
+          </p>
+        </div>
       </div>
-      <span
-        className={`text-[0.7rem] self-end text-[#A1A4A9] relative ${isSelf ? 'left-2' : 'right-2'}`}
-      >
-        {message.timestamp}
-      </span>
     </div>
   )
 }
